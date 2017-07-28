bc90e8e732aad4b4c19d56ff5a74dac6c28a7e07
Hello Tunmbi <hellotunmbi@gmail.com>
2017-07-28 15:58:36 +0100

    adding search functionality

--- js/script.js
+++ js/script.js
@@ -8,6 +8,11 @@ app.config(function($routeProvider) {
             controller: 'trendingCtrl'
         })
 
+        .when("/search", {
+            templateUrl: 'views/search.html',
+            controller: 'searchCtrl'
+        })
+
         .otherwise({
             templateUrl: 'views/home.html'
         })
@@ -26,3 +31,23 @@ app.controller('trendingCtrl', ['$scope', '$http', function($scope, $http) {
 }]);
 
 
+app.controller('searchCtrl', ['$scope', '$http', function($scope, $http) {
+
+    $scope.getGiphies = function() {
+        console.log("someone clicked me");
+
+        $scope.message = "hi there";
+
+    }
+
+    
+
+    // $http.get('http://api.giphy.com/v1/gifs/search?api_key=3a5d40cc20b943719f53a35ec3b2abff' + '& q=cheese')
+    //     .then(function(response) {
+    //         $scope.result = response.result;
+    //     },
+    //      function(error){ 
+
+    //      })
+}]);
+//end of searchCtrl
